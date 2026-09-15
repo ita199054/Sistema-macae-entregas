adfe6e1d305b47a69e35d0d8a9ae57f6e252f96d
ita199054 <italorossi.generali@gmail.com>
2026-09-15 05:10:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 // LUGAR SEGURO DA SUA CHAVE - COLE DENTRO DAS ASPAS
-const MINHA_CHAVE = "";
-const TOKEN_PAGAMENTO = "";
+const MINHA_CHAVE = "AIzaSyB..."; // sua chave do Google Maps
+const TOKEN_PAGAMENTO = "22997450131"; // seu Pix
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2vEov-Mac2JHhl_CkfEL7hvmwQBuIVeo",
+  authDomain: "anjos-flash-deliver-brasil.firebaseapp.com",
+  projectId: "anjos-flash-deliver-brasil",
+  storageBucket: "anjos-flash-deliver-brasil.firebasestorage.app",
+  messagingSenderId: "1081152420523",
+  appId: "1:1081152420523:web:5d730a03455699956c95ee"
+};
